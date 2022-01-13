a62aa02d5d13d3f2127dece8f25a5c9ad1983171
Adjuvant <tom@layeraudio.com>
2022-01-13 16:13:07 +0000

    office updates

---
+++ Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int age = 37; // Add your own here in the assignment
+        string name = "Tom"; // Likewise, enter yours here
+        print("Hello " + name);
+        print("You are " + age + " years old");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/_TutorialResources/01-BehaviourAndVariables/Variables.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Variables : MonoBehaviour
+{
+	int myPinNumber = 1234;
+	float timeToMakeDecision = 495.5f; // note the f to indicate its a float type
+	bool tomIsALiar = true;
+	bool isFalse, isNotTrue; // Un valued default to false;
+	int thisIsZero;
+	float thisIsAlsoZero;
+	string coreLifeSkill = "flattery";
+}
+
+
